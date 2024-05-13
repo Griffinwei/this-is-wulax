73aae19e828285f9d24f625922f2eaa78c5a6fc1
Griffin Wei <g.t.wei@wustl.edu>
2024-05-13 03:16:06 -0500

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './washulacrosselogo.jpg';
 import './App.css';
 
 function App() {
@@ -6,16 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reafct
+        <a>
+          Coming soon...
         </a>
       </header>
     </div>
