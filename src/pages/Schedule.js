8581a6f2b0adff9772e676040f773305c1a05b56
Griffin Wei <g.t.wei@wustl.edu>
2024-08-18 16:56:27 -0500

    Website is fully functional

---
+++ src/pages/Schedule.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Schedule = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "centre",
+                alignItems: "centre",
+                height: "100vh",
+                backgroundColor: "#e3e3e3",
+                margin: "24px",
+                padding: "18px"
+            }}
+        >
+            <h1>Coming soon...</h1>
+        </div>
+    );
+};
+
+export default Schedule;
